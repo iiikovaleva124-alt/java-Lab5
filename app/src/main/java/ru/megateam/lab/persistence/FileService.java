0268a9f5b7771ed9da28f0e8b9b82445403f714c
iiikovaleva124-alt <iiikovaleva124@gmail.com>
2026-04-23 19:17:34 +0300

    подключен InstrumentService

--- app/src/main/java/ru/megateam/lab/persistence/FileService.java
+++ app/src/main/java/ru/megateam/lab/persistence/FileService.java
@@ -1,22 +1,28 @@
 package ru.megateam.lab.persistence;
 
+import ru.megateam.lab.domain.Sample;
 import ru.megateam.lab.repository.IncidentRepository;
+import ru.megateam.lab.service.InstrumentService;
 import ru.megateam.lab.service.SampleService;
 
 import java.util.List;
+import java.util.Map;
 
 public class FileService {
     private final IncidentRepository incidentRepository;
     private final SampleService sampleService;
+    private final InstrumentService instrumentService;
     private final FileStorage fileStorage;
     private final FileValidator fileValidator;
 
     public FileService(IncidentRepository incidentRepository,
                        SampleService sampleService,
+                       InstrumentService instrumentService,
                        FileStorage fileStorage,
                        FileValidator fileValidator) {
         this.incidentRepository = incidentRepository;
         this.sampleService = sampleService;
+        this.instrumentService = instrumentService;
         this.fileStorage = fileStorage;
         this.fileValidator = fileValidator;
     }
@@ -25,6 +31,7 @@ public class FileService {
         AppState state = new AppState(
                 incidentRepository.getAll(),
                 sampleService.getAll(),
+                instrumentService.getAll(),
                 incidentRepository.getAllCommentsMap()
         );
 
@@ -43,6 +50,7 @@ public class FileService {
 
         incidentRepository.replaceAll(loaded.getIncidents());
         sampleService.replaceAll(loaded.getSamples());
+        instrumentService.replaceAll(loaded.getInstruments());
         incidentRepository.replaceAllComments(loaded.getComments());
     }
 }
