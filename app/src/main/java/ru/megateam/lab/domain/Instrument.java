5a37c1200019b922d565709d969c37aa50809459
iiikovaleva124-alt <iiikovaleva124@gmail.com>
2026-04-23 19:20:16 +0300

    создан отдельный класс Instrument

---
+++ app/src/main/java/ru/megateam/lab/domain/Instrument.java
@@ -0,0 +1,14 @@
+package ru.megateam.lab.domain;
+
+public class Instrument {
+    private final long id;
+    private final String name;
+
+    public Instrument(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() { return id; }
+    public String getName() { return name; }
+}
